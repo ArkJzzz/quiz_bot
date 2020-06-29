a85ebcd8d75575fc76a11e69f68843de39783623
ArkJzzz <arkjzzz@gmail.com>
2020-06-29 19:03:09 +0300

    реализован бот VK

--- bot-vk.py
+++ bot-vk.py
@@ -11,6 +11,8 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 
+import redis
+
 import settings
 import quiz_tools
 import redis_tools
@@ -19,15 +21,81 @@ import redis_tools
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.DEBUG)
 
+DATABASE = redis.Redis(
+    host=settings.redis_host, 
+    port=settings.redis_port, 
+    db=settings.redis_db_number, 
+    password=settings.redis_password,
+)
+
+
+def handle_new_question(event, vk, keyboard):
+    question_card_number = quiz_tools.get_random_question_card_number(DATABASE)
+    question_card = redis_tools.get_value_from_database(
+            key=question_card_number, 
+            database=DATABASE,
+        )
+    chat_id = event.user_id
+    quiz_tools.add_user_to_database(
+            chat_id=chat_id,
+            source='vk',
+            value=question_card_number,
+            database=DATABASE,
+        )
+    logger.debug(
+        'user: {}\nquestion_card_number: {}\n{}'.format(
+            chat_id,
+            question_card_number,
+            question_card,
+        )
+    )
+    question = question_card['question']
+
+    vk.messages.send(
+        peer_id = chat_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=question, 
+    )
+
+def handle_capitulate(event, vk, keyboard):
+    chat_id = event.user_id
+    last_asked_question = quiz_tools.get_last_asked_question(
+        chat_id=chat_id, 
+        source='vk', 
+        database=DATABASE,
+        )
+    answer = quiz_tools.get_long_answer(last_asked_question, DATABASE)
+    vk.messages.send(
+        peer_id = chat_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=answer, 
+    )
+
+def handle_answer_attempt(event, vk, keyboard):
+    chat_id = event.user_id
+    user_answer = event.text
+    verdict = quiz_tools.evaluate_answer(
+            user_answer=user_answer, 
+            chat_id=chat_id, 
+            source='vk', 
+            database=DATABASE,
+        )
+    vk.messages.send(
+        peer_id = chat_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=verdict,
+    )
+
 
 def send_text_message(event, vk, keyboard):
-
     vk.messages.send(
         peer_id = event.user_id,
         random_id=get_random_id(),
         keyboard=keyboard.get_keyboard(),
         message=event.text,
-        
     )
     
 
@@ -51,10 +119,12 @@ def main():
     try:
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                logger.debug('Новое сообщение:')
-                logger.debug('Для меня от: {}'.format(event.user_id))
-                logger.debug('Текст:{}'.format(event.text))
-                send_text_message(event, vk, keyboard)
+                if event.text == 'Новый вопрос':
+                    handle_new_question(event, vk, keyboard)
+                elif event.text == 'Сдаться':
+                    handle_capitulate(event, vk, keyboard)
+                else: 
+                    handle_answer_attempt(event, vk, keyboard)
     except KeyboardInterrupt:
         logger.info('Бот остановлен')
     except Exception  as err:
