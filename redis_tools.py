7df2bde859dd8954b5439b48f8fc2b957db9a52a
ArkJzzz <arkjzzz@gmail.com>
2020-07-29 15:07:04 +0300

    подчистил лишнее

--- redis_tools.py
+++ redis_tools.py
@@ -30,30 +30,6 @@ def connect_to_redis():
     return database
 
 
-def add_card_to_database(question_card, database):
-    value = json.dumps(question_card)
-    database.rpush('question_cards', value)
-    logger.info('БД: добавлена запись: {}'.format(question_card))
-
-
-def get_keys_from_database(database, pattern='*'):
-    keys = database.keys(pattern=pattern)
-    decoded_keys = []
-    for key in keys:
-        decoded_key = key.decode('utf-8')
-        decoded_keys.append(decoded_key)
-
-    return decoded_keys
-
-
-def get_value_from_database(key, database):
-    logger.debug(key)
-    value = database.get(key)
-    value = json.loads(value)
-
-    return value
-
-
 def clear_database(database):
     keys = database.keys()
     for key in keys:
